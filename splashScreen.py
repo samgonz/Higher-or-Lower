922e8084933dfc20e3f98aa98b2a2c328a085b80
samgonz <doomteam1@gmail.com>
2022-10-05 00:04:11 -0500

    added additional data

---
+++ splashScreen.py
@@ -0,0 +1,19 @@
+logo = '''
+
+█░█ █ █▀▀ █░█ █▀▀ █▀█
+█▀█ █ █▄█ █▀█ ██▄ █▀▄
+
+█░░ █▀█ █░█░█ █▀▀ █▀█
+█▄▄ █▄█ ▀▄▀▄▀ ██▄ █▀▄
+'''
+
+instructions = '''
+Welcome to Higher or Lower.
+The goal of the game is to guess who has the higher follower count.
+Select [1] for the top selection and [2] for the bottom selection.
+      '''
+      
+versus = '''
+█░█ █▀
+▀▄▀ ▄█
+'''
